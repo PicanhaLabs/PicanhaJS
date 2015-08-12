355dc9ba5c02b7269de366bae1116d8016eea82f
Renie Siqueira <renie.webdev@gmail.com>
2015-08-11 23:21:32 -0300

    Draft support.

--- bin/builder.js
+++ bin/builder.js
@@ -8,7 +8,7 @@ var fs 			= require('fs'),
 function Builder(config) {
 	var options = config || {};
 
-	this.paths = {
+	this.paths	= {
 		lib: options.libpath,	
 		cli: options.clientpath	
 	};
@@ -37,15 +37,15 @@ Builder.prototype = {
 		this.copyFn = fn;
 	},
 
-	setParameters: function(params){
+	setParameters: function(params) {
 		this.parameters = params;
 	},
 
-	setFrontMatterCompiler: function( compiler ){
+	setFrontMatterCompiler: function(compiler) {
 		this.frontMatterCompiler = compiler;
 	},
 
-	setPostCompiler: function( compiler ){
+	setPostCompiler: function(compiler) {
 		this.postCompiler = compiler;
 	},
 
@@ -53,17 +53,16 @@ Builder.prototype = {
 	 * Setter of template compiler
 	 * This method takes advantage of the object and compiles the default templates
 	 */
-	setTemplateCompiler: function( compiler ){
+	setTemplateCompiler: function(compiler) {
 		var me = this, toCompile = me.parameters.template.layouts;
 
 		me.templateCompiler = compiler;
 
-		toCompile.forEach(function(current){
+		toCompile.forEach(function(current) {
 			(function(name) {
-				fs.readFile(path.join(me.parameters.template.path, name + '.html'), 'utf8', function(error, data){
-					if( error ) {
+				fs.readFile(path.join(me.parameters.template.path, name + '.html'), 'utf8', function(error, data) {
+					if (error)
 						throw error;
-					}
 		
 					me.templates[name] = me.templateCompiler(data);
 				});
@@ -77,9 +76,9 @@ Builder.prototype = {
 	getFiles: function() {
 		var me = this;
 
-		return new Promise(function(resolve, reject){
+		return new Promise(function(resolve, reject) {
 			
-			fs.readdir(me.parameters.posts.source, function(readError, files){
+			fs.readdir(me.parameters.posts.source, function(readError, files) {
 				if( readError )
 					reject(readError);
 					
@@ -92,7 +91,7 @@ Builder.prototype = {
 	buildPosts: function(files){
 		var me = this, p = [];
 
-		return new Promise(function(resolve){
+		return new Promise(function(resolve) {
 			
 			files.forEach(function(file) {
 				p.push(me.makePost(path.join(me.parameters.posts.source, file)));			
@@ -108,73 +107,77 @@ Builder.prototype = {
 	 * Make the post compiling with the passed compiler
 	 */
 	makePost: function( filePath ) {
-		var me = this, newFileName, globals, content, filename, result, ispage, page;
+		var me = this, newFileName, globals, content, filename, result, ispage, page, isdraft;
 
 		return new Promise(function(resolve) {
 
-			fs.stat(filePath, function(statsError, stats){
+			fs.stat(filePath, function(statsError, stats) {
 
-				if( stats.isDirectory() )
+				if (stats.isDirectory())
 					return true;
 					
-				if( statsError ) {
+				if (statsError)
 					throw statsError;
-				}
+				
 					
-				fs.readFile(filePath, 'utf8', function(fileErr, data){
-					if (fileErr) {
+				fs.readFile(filePath, 'utf8', function(fileErr, data) {
+					
+					if (fileErr)
 						throw fileErr;
-					}
-										
-					content		= me.frontMatterCompiler(data);
-					result		= content.attributes;
-					ispage		= result.template !== 'post' && result.template;
-					globals		= {
-						baseurl: ispage ? '../' : '../../../../',
-						authors: me.authors
-					};
+					
 
-					result.date = result.date ? moment(result.date, me.parameters.posts.dateformat) : moment();
+					content				= me.frontMatterCompiler(data);
+					result				= content.attributes;
+					ispage				= result.template !== 'post' && result.template;
+					isdraft				= result.draft;
+					globals				= {
+											baseurl : ispage ? '../' : '../../../../',
+											authors : me.authors
+										};
 
-					result.author	= me.findAuthor(result.author);
-
+					result.date			= result.date ? moment(result.date, me.parameters.posts.dateformat) : moment();
+					result.author		= me.findAuthor(result.author);
 					result.creationdate = result.date.valueOf();
+					result.creation		= utils.formatData(new Date(result.creationdate));
 
-					result.creation = utils.formatData(new Date(result.creationdate));
+					page				= me.templateCompiler( me.postCompiler(content.body) );
 						
-					page = me.templateCompiler( me.postCompiler(content.body) );
-						
-					result.body		= page({ globals: globals });
-					
-					result.excerpt	= result.excerpt ? '<p>' + result.excerpt + '</p>' : result.body.match(/<p>.+<\/p>/i)[0];
+					result.body			= page({ globals: globals });
+					result.excerpt		= result.excerpt ? '<p>' + result.excerpt + '</p>' : result.body.match(/<p>.+<\/p>/i)[0];
 
-					filename	= path.basename(filePath).split('.')[0];
-					
-					if( !ispage ) {
-						newFileName = me.makePostPath(filename, result.date);
-					} else {
-						newFileName = me.makePagePath(filename, result.date);
-					}
 
-					result.url	= newFileName.replace( path.normalize(me.parameters.dist), '' ).replace(/\\/g, '/');
+					filename			= path.basename(filePath).split('.')[0];
 
 					
-					console.log('\x1b[36mCreating: \x1b[0m' + newFileName);
+					if (isdraft)
+						filename += '-draft';
+					
 
-					if( !ispage ) {
+					if (!ispage)
+						newFileName		= me.makePostPath(filename, result.date);
+					else
+						newFileName		= me.makePagePath(filename, result.date);
+
+
+					result.url			= newFileName.replace( path.normalize(me.parameters.dist), '' ).replace(/\\/g, '/');
+
+					
+					console.log('\x1b[36mCreating ' + (isdraft ? 'DRAFT' : '') + ': \x1b[0m' + newFileName);
+
+
+					if( !ispage && !isdraft )
 						me.postsData.push(result);
-					}
+					
 					
 					var tpl		= !ispage ? me.templates.post : me.templates[result.template],
 						html	= tpl({
-							post: result,
-							globals: globals
+							post	: result,
+							globals : globals
 						});				
 					
 					fs.writeFile(newFileName, html, function(writeError){
-						if( writeError ) {
+						if (writeError)
 							throw writeError;
-						}
 					});
 					
 					resolve();
@@ -188,7 +191,7 @@ Builder.prototype = {
 	 * Mount and create the post path (if it does not exist)
 	 */
 	makePostPath: function(filename, momentDate) {
-		var me = this, newFilePath, newFileName;
+		var me		= this, newFilePath, newFileName;
 
 		newFilePath = me.parameters.posts.dist.path.replace(':year', momentDate.year());
 		newFilePath = newFilePath.replace(':month', utils.padNumber(momentDate.month() + 1, 2));
@@ -208,7 +211,7 @@ Builder.prototype = {
 	 * Mount and create the page path (if it does not exist)
 	 */
 	makePagePath: function(filename, status) {
-		var me = this, newFilePath, newFileName;
+		var me		= this, newFilePath, newFileName;
 
 		newFilePath = path.join(me.parameters.dist, filename);
 		newFileName = path.join(newFilePath, me.parameters.posts.dist.name);
@@ -221,12 +224,11 @@ Builder.prototype = {
 	createHome: function() {
 		var me = this;
 
-		return new Promise(function(resolve, reject){
-			fs.readFile(path.join(me.parameters.template.path, me.parameters.template.home), 'utf8', function(fileErr, data){
+		return new Promise(function(resolve, reject) {
+			fs.readFile(path.join(me.parameters.template.path, me.parameters.template.home), 'utf8', function(fileErr, data) {
 				
-				if( fileErr ) {
+				if( fileErr )
 					throw fileErr;
-				}
 				
 				me.postsData = me.postsData.sort(function(post1, post2) {
 					return post2.creationdate - post1.creationdate;
@@ -241,12 +243,11 @@ Builder.prototype = {
 						}
 					});
 				
-				console.log('\x1b[36mCreating:\x1b[0m home');
+				console.log('\x1b[36mCreating :\x1b[0m home');
 				
-				fs.writeFile(path.join(me.parameters.dist, 'index.html'), result, function(writeError){
-					if( writeError ) {
+				fs.writeFile(path.join(me.parameters.dist, 'index.html'), result, function(writeError) {
+					if( writeError )
 						throw writeError;
-					}
 				});
 
 				resolve();
@@ -261,8 +262,8 @@ Builder.prototype = {
 		me.authors	= me.parameters.authors || [];
 
 		me.authors.forEach(function(el) {
-			var md5		= crypto.createHash('md5');
-			hash = md5.update(el.email).digest("hex");
+			var md5	= crypto.createHash('md5');
+			hash 	= md5.update(el.email).digest("hex");
 
 			for (var size in me.parameters.defaultAuthorSizes) {
 				el['pathAvatar-' + size] = 'http://www.gravatar.com/avatar/' + hash + '?d=identicon&s=' + me.parameters.defaultAuthorSizes[size];
@@ -271,8 +272,7 @@ Builder.prototype = {
 	},
 
 	findAuthor : function(email) {
-		var me		= this,
-			result, md5, hash;
+		var me		= this, result, md5, hash;
 
 		result = me.authors.filter(function(el) {
 			return el.email === email;
@@ -287,9 +287,9 @@ Builder.prototype = {
 
 			hash			= md5.update(email).digest("hex");
 
-			for (var size in me.parameters.defaultAuthorSizes) {
+			for (var size in me.parameters.defaultAuthorSizes)
 				result['pathAvatar-' + size] = 'http://www.gravatar.com/avatar/' + hash + '?d=identicon&s=' + me.parameters.defaultAuthorSizes[size];
-			}
+
 		} else {
 			result = result[0];
 		}
@@ -303,6 +303,7 @@ Builder.prototype = {
 		me.createAuthors();
 
 		console.log('\x1b[36mCleaning\x1b[0m');
+
 		utils.deleteFolderRecursive(me.parameters.dist);
 
 		me.getFiles()
