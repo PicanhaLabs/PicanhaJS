9203aa7cd4ae1858e0bf15e6dc836cdb92d396cf
Gabriel Alan <gaba.alan@gmail.com>
2015-08-25 23:50:37 -0300

    More Builder Tests

--- test/builderTest.js
+++ test/builderTest.js
@@ -1,5 +1,6 @@
 var assert = require('chai').assert,
 	Promise = require('promise'),
+	path 	= require('path'),
 	defaultparams = require('../bin/picanha.json');
 
 var Creator = require('../bin/builder');
@@ -45,6 +46,15 @@ describe('Builder', function(){
 					done();
 				});
 			});
+			
+			it('should reject a promise if path does not exists', function(done){
+				var old = instance.parameters.posts.source;
+				instance.parameters.posts.source = './errorpath';
+				instance.getFiles().catch(function(){
+					done();
+				});
+				instance.parameters.posts.source = old;
+			});
 		});
 		
 		describe('setTemplateCompiler', function(){
@@ -66,6 +76,19 @@ describe('Builder', function(){
 			});
 		});
 		
+		describe('readStoreTemplate', function(){
+			it('read and compile a template', function(){
+				var fn = function(){
+					instance.readStoreTemplate('errorfile');
+					
+					if( !instance.templates.errorfile )
+						throw new Error('template not found');	
+				};
+				
+				assert.throw(fn, Error);
+			});
+		});
+		
 		describe('setFrontMatterCompiler', function(){
 			it('receives a function to compile front-matter', function(){
 				var frontMatterCompiler = function(data) {
@@ -75,6 +98,43 @@ describe('Builder', function(){
 				instance.setFrontMatterCompiler(frontMatterCompiler);
 				
 				assert.strictEqual(instance.frontMatterCompiler, frontMatterCompiler);
+			});
+		});
+		
+		describe('setCopyFn', function(){
+			it('receives a function to copy folders and files', function(){
+				instance.setCopyFn(function(){
+					return true;
+				});
+			});
+		});
+		
+		describe('setPostCompiler', function(){
+			it('should receive an function to compile markdown', function(){
+				instance.setPostCompiler(function(md){
+					assert.isString(md);
+					return md;
+				});
+			});
+		});
+		
+		describe('makePostPath', function(){
+			it('should create the post folder path', function(){
+				instance.parameters.posts.dist.path = ":year/:month/:day/:name/";
+				
+				assert.strictEqual(
+					instance.makePostPath('test', {year:2015, month:0, date:1}),
+					path.normalize('_build/2015/01/01/test/index.html')
+				);
+			});
+		});
+		
+		describe('makePagePath', function(){
+			it('should create the page folder path', function(){
+				assert.strictEqual(
+					instance.makePagePath('test'),
+					path.normalize('_build/test/index.html')
+				);
 			});
 		});
 	});
