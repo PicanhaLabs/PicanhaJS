365698513cd0283fc7788c179716b0305349da42
Renie Siqueira <renie.webdev@gmail.com>
2015-09-17 13:29:24 -0300

    Start testing Arguments class.

---
+++ test/ArgumentsTest.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var assert				= require('chai').assert;
+var ArgumentsController = require('../bin/lib_1.1/ArgumentsController');
+
+var defaultArgs = ['node', 'picanha'];
+
+
+
+describe('ArgumentsController', function() {
+
+	it('Contructor', function() {
+		let AC;
+
+		defaultArgs.push('grill');
+
+		AC = new ArgumentsController(defaultArgs);
+
+		assert.isDefined(AC.router);
+
+		defaultArgs.pop();
+	});
+
+});
