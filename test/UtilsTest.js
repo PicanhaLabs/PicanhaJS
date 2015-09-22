bde32e1ecb19da42b3197a0ac0edc966df16ef76
Renie Siqueira <renie.webdev@gmail.com>
2015-09-21 22:39:15 -0300

    Utils test file

---
+++ test/UtilsTest.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+// dependencies
+var assert				= require('chai').assert;
+
+
+// classes
+var Utils				= require('../bin/lib_1.1/Utils');
+
+
+
+describe('Utils', function() {
+
+});
