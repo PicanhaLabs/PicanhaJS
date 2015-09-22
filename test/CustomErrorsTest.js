34726a54e89cf4cf1b0910352e806664773ada91
Renie Siqueira <renie.webdev@gmail.com>
2015-09-21 22:37:52 -0300

    Custom Error test file

---
+++ test/CustomErrorsTest.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+// dependencies
+var assert				= require('chai').assert;
+
+
+// classes
+var CustomErrors		= require('../bin/lib_1.1/CustomErrors');
+
+
+
+describe('CustomErrors', function() {
+
+});
