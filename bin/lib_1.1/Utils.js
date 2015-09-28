a15d07a41baf37da901b71c299231641cad5b273
Renie Siqueira <renie.webdev@gmail.com>
2015-09-27 22:35:56 -0300

    Arrow functions for Utils

--- bin/lib_1.1/Utils.js
+++ bin/lib_1.1/Utils.js
@@ -4,18 +4,19 @@
 var CustomErrors	= require('./CustomErrors'),
 	ncp				= require('ncp').ncp;
 
+
 var Utils = {
-	parameterCheck : function(type, arg) {
+	parameterCheck : function (type, arg){
 		if (!arg && arg !== 0 && arg !== false && arg !== '')
 			throw new CustomErrors.ParameterNotFound();
 		else if (!(arg instanceof type))
 			throw new TypeError("Expected an " + type.toString() + " as parametes for this constructor.");
 	},
 
-	recursiveCopy : function (src, dest) {
-		return new Promise(function(resolve, reject) {
-			ncp(src, dest, function(error) {
-				if( error ){
+	recursiveCopy : function (src, dest){
+		return new Promise((resolve, reject) => {
+			ncp(src, dest, (error) => {
+				if (error) {
 					reject(error);
 					return false;
 				}
